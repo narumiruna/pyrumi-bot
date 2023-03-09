58565c8de303a207bac473047c451ff7f3f871f7
なるみ <toucans-cutouts0f@icloud.com>
2023-03-10 00:30:56 +0800

    fix

--- main.py
+++ main.py
@@ -13,58 +13,52 @@ BOT_TOKEN = os.environ.get('TELEGRAM_BOT_TOKEN')
 openai.api_key = os.environ.get('OPENAI_API_KEY')
 
 
+def join(resposne_messages):
+    return '\n'.join([message['content'] for message in resposne_messages])
+
+
 class ChatGPT:
 
     def __init__(self):
         self.dialogues = {}
 
     async def create(self, messages):
-        return await openai.ChatCompletion.acreate(model="gpt-3.5-turbo", messages=messages)
+        response = await openai.ChatCompletion.acreate(model="gpt-3.5-turbo", messages=messages)
+
+        resposne_messages = []
+        for i, choice in enumerate(response.choices):
+            logger.info('#{} message: {}', i, choice.message)
+            resposne_messages.append(dict(choice.message))
+        return resposne_messages
 
     async def reply(self, update: Update, context: ContextTypes.DEFAULT_TYPE):
         reply_id = update.message.reply_to_message.message_id
         if reply_id not in self.dialogues.keys():
             logger.info('reply_id: {} not exists', reply_id)
             return
-        else:
-            messages = self.dialogues[reply_id]
-        messages.append({'role': 'user', 'content': update.message.text})
 
-        response = await self.create(messages)
-
-        response_contents = []
-        for i, choice in enumerate(response.choices):
-            logger.info('#{} chatgpt: {}', i, choice.message.content)
-            logger.info('#{} message: {}', i, choice.message)
-            messages.append(dict(choice.message))
-            response_contents.append(choice.message.content)
+        messages = self.dialogues[reply_id] + [{'role': 'user', 'content': update.message.text}]
+        resposne_messages = await self.create(messages)
 
         new_message = await context.bot.send_message(chat_id=update.effective_chat.id,
-                                                     text='\n'.join(response_contents),
+                                                     text=join(resposne_messages),
                                                      reply_to_message_id=update.message.id)
 
-        self.dialogues[new_message.message_id] = messages
+        self.dialogues[new_message.message_id] = messages + resposne_messages
 
         logger.info('messages: {}', messages)
 
     async def start_gpt(self, update: Update, context: ContextTypes.DEFAULT_TYPE):
-        messages = [{'role': 'user', 'content': update.message.text}]
-        response = await openai.ChatCompletion.acreate(model="gpt-3.5-turbo", messages=messages)
-
-        response_contents = []
-        for i, choice in enumerate(response.choices):
-            logger.info('#{} chatgpt: {}', i, choice.message.content)
-            logger.info('#{} message: {}', i, choice.message)
-            messages.append(dict(choice.message))
-            response_contents.append(choice.message.content)
+        messages = [{'role': 'user', 'content': update.message.text.rstrip('/gpt')}]
+        resposne_messages = await self.create(messages)
 
         new_message = await context.bot.send_message(chat_id=update.effective_chat.id,
-                                                     text='\n'.join(response_contents),
+                                                     text=join(resposne_messages),
                                                      reply_to_message_id=update.message.id)
 
         logger.info('new message id: {}', new_message.message_id)
         logger.info('thread id: {}', new_message.message_thread_id)
-        self.dialogues[new_message.message_id] = messages
+        self.dialogues[new_message.message_id] = messages + resposne_messages
 
 
 if __name__ == '__main__':
