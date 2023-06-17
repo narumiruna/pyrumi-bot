e1e456b71b9bfd472fbf6fa84cbd2d0a6b2d2519
なるみ <toucans-cutouts0f@icloud.com>
2023-06-18 03:16:27 +0800

    feat: implement PyRumi bot with echo command
    - Create a new file `bot.py` for the PyRumi bot
    - Implement an `echo` command that repeats the user's message
    - Set up the `start_bot` function to initialize bot token and application
    - Use `CommandHandler` to handle the `echo` command and run polling
    
    Signed-off-by: なるみ <toucans-cutouts0f@icloud.com>

---
+++ pyrumi/bot.py
@@ -0,0 +1,29 @@
+import os
+
+from dotenv import load_dotenv
+from loguru import logger
+from telegram import Update
+from telegram.ext import ApplicationBuilder
+from telegram.ext import CommandHandler
+from telegram.ext import ContextTypes
+
+ECHO_COMMAND = 'echo'
+
+
+async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE):
+
+    logger.info('message: {}', update.message)
+    await context.bot.send_message(chat_id=update.effective_chat.id,
+                                   text=update.message.text.lstrip('/' + ECHO_COMMAND),
+                                   reply_to_message_id=update.message.id)
+
+
+def start_bot():
+    load_dotenv()
+
+    bot_token = os.environ.get('BOT_TOKEN')
+    application = ApplicationBuilder().token(bot_token).build()
+
+    echo_handler = CommandHandler(ECHO_COMMAND, echo)
+    application.add_handler(echo_handler)
+    application.run_polling()
