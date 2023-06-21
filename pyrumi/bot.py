b9968eda22c0e8d14721a38c89f1d23d887d0dbb
なるみ <toucans-cutouts0f@icloud.com>
2023-06-21 22:55:02 +0800

    fix: refactor error handling function for better formatting and handling
    - Add an error handling function with message formatting and traceback
    - Remove the old error handling function and replace it with the new one
    
    Signed-off-by: なるみ <toucans-cutouts0f@icloud.com>

--- pyrumi/bot.py
+++ pyrumi/bot.py
@@ -1,8 +1,12 @@
+import html
+import json
 import os
+import traceback
 
 from dotenv import load_dotenv
 from loguru import logger
 from telegram import Update
+from telegram.constants import ParseMode
 from telegram.ext import ApplicationBuilder
 from telegram.ext import BaseHandler
 from telegram.ext import CommandHandler
@@ -24,6 +28,30 @@ async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE):
                                    reply_to_message_id=update.message.id)
 
 
+async def error_handler(update: object, context: ContextTypes.DEFAULT_TYPE) -> None:
+    """Log the error and send a telegram message to notify the developer."""
+    # Log the error before we do anything else, so we can see it even if something breaks.
+    logger.error("Exception while handling an update:", exc_info=context.error)
+
+    # traceback.format_exception returns the usual python message about an exception, but as a
+    # list of strings rather than a single string, so we have to join them together.
+    tb_list = traceback.format_exception(None, context.error, context.error.__traceback__)
+    tb_string = "".join(tb_list)
+
+    # Build the message with some markup and additional information about what happened.
+    # You might need to add some logic to deal with messages longer than the 4096 character limit.
+    update_str = update.to_dict() if isinstance(update, Update) else str(update)
+    message = (f"An exception was raised while handling an update\n"
+               f"<pre>update = {html.escape(json.dumps(update_str, indent=2, ensure_ascii=False))}"
+               "</pre>\n\n"
+               f"<pre>context.chat_data = {html.escape(str(context.chat_data))}</pre>\n\n"
+               f"<pre>context.user_data = {html.escape(str(context.user_data))}</pre>\n\n"
+               f"<pre>{html.escape(tb_string)}</pre>")
+
+    # Finally, send the message
+    await context.bot.send_message(chat_id=update.effective_chat.id, text=message, parse_mode=ParseMode.HTML)
+
+
 def start_bot():
     load_dotenv('.env')
 
@@ -33,10 +61,8 @@ def start_bot():
 
     application = ApplicationBuilder().token(bot_token).build()
 
-    def handle_error(update: Update, context: ContextTypes.DEFAULT_TYPE):
-        logger.error('error: {}', context.error)
+    application.add_error_handler(error_handler)
 
-    application.add_error_handler(BaseHandler(handle_error))
     # echo command
     application.add_handler(CommandHandler(ECHO_COMMAND, echo))
 
