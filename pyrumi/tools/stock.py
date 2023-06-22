62920016de5c8fe61f39fa4876765993f83742ad
なるみ <toucans-cutouts0f@icloud.com>
2023-06-23 01:58:15 +0800

    docs: improve tool descriptions and remove flake8 ignore directive
    - Update the description of three stock tools to include more details about the required inputs
    - Remove the `ignore` directive for flake8 in the `setup.cfg` file
    
    Signed-off-by: なるみ <toucans-cutouts0f@icloud.com>

--- pyrumi/tools/stock.py
+++ pyrumi/tools/stock.py
@@ -19,7 +19,8 @@ class StockPriceCheckInput(BaseModel):
 
 class StockPriceTool(BaseTool):
     name = "get_stock_ticker_price"
-    description = "Useful for when you need to find out the price of stock. You should input the stock ticker used on the yfinance API"
+    description = ("Useful for when you need to find out the price of stock. "
+                   "You should input the stock ticker used on the yfinance API.")
 
     def _run(self, stockticker: str):
         price_response = get_stock_price(stockticker)
@@ -42,7 +43,9 @@ class StockChangePercentageCheckInput(BaseModel):
 
 class StockPercentageChangeTool(BaseTool):
     name = "get_price_change_percent"
-    description = "Useful for when you need to find out the percentage change in a stock's value. You should input the stock ticker used on the yfinance API and also input the number of days to check the change over"
+    description = ("Useful for when you need to find out the percentage change in a stock's value. "
+                   "You should input the stock ticker used on the yfinance API "
+                   "and also input the number of days to check the change over.")
 
     def _run(self, stockticker: str, days_ago: int):
         price_change_response = get_price_change_percent(stockticker, days_ago)
@@ -63,7 +66,9 @@ class StockBestPerformingInput(BaseModel):
 
 class StockGetBestPerformingTool(BaseTool):
     name = "get_best_performing"
-    description = "Useful for when you need to the performance of multiple stocks over a period. You should input a list of stock tickers used on the yfinance API and also input the number of days to check the change over"
+    description = ("Useful for when you need to the performance of multiple stocks over a period."
+                   "You should input a list of stock tickers used on the yfinance API "
+                   "and also input the number of days to check the change over.")
 
     def _run(self, stocktickers: List[str], days_ago: int):
         price_change_response = get_best_performing(stocktickers, days_ago)
