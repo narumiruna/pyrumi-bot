37de4ed245332b6af4b39e4d6923857f30552b50
なるみ <toucans-cutouts0f@icloud.com>
2023-06-23 00:51:56 +0800

    refactor: simplify chatbot code and remove unnecessary imports/handlers
    - Remove the `echo` command
    - Remove the `chatgpt_bot` import and handlers
    - Comment out `chatgpt_bot` initialization and handlers
    - Simplify `error_handler` function
    
    Signed-off-by: なるみ <toucans-cutouts0f@icloud.com>

--- pyrumi/bot.py
+++ pyrumi/bot.py
@@ -13,19 +13,10 @@ from telegram.ext import ContextTypes
 from telegram.ext import MessageHandler
 from telegram.ext import filters
 
-from .chatgpt_bot import ChatGPTBot
 from .langchain_bot import LangChainBot
 
 ECHO_COMMAND = 'echo'
 DEVELOPER_CHAT_ID = 102825484
-
-
-async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE):
-
-    logger.info('message: {}', update.message)
-    await context.bot.send_message(chat_id=update.effective_chat.id,
-                                   text=update.message.text.lstrip('/' + ECHO_COMMAND),
-                                   reply_to_message_id=update.message.id)
 
 
 async def error_handler(update: object, context: ContextTypes.DEFAULT_TYPE) -> None:
@@ -62,14 +53,6 @@ def start_bot():
     application = ApplicationBuilder().token(bot_token).build()
 
     application.add_error_handler(error_handler)
-
-    # echo command
-    application.add_handler(CommandHandler(ECHO_COMMAND, echo))
-
-    # add chatgpt bot
-    # chatgpt_bot = ChatGPTBot.from_env()
-    # application.add_handler(CommandHandler('g', chatgpt_bot.start))
-    # application.add_handler(MessageHandler(filters.REPLY & filters.TEXT & (~filters.COMMAND), chatgpt_bot.reply))
 
     # add langchain bot
     langchain_bot = LangChainBot.from_env()
