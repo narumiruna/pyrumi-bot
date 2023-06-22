5c651082a7b78e106dd5203cf9d6397cafa8dfcf
なるみ <toucans-cutouts0f@icloud.com>
2023-06-23 00:34:49 +0800

    chore: add Playwright support to agent script
    - Add Playwright to the project dependencies in `pyproject.toml`
    - Add Playwright tools to the `agent.py` script
    
    Signed-off-by: なるみ <toucans-cutouts0f@icloud.com>

--- agent.py
+++ agent.py
@@ -3,7 +3,14 @@ from langchain.agents import AgentType
 from langchain.agents import initialize_agent
 from langchain.chat_models import ChatOpenAI
 from langchain.tools import ArxivQueryRun
+from langchain.tools import ClickTool
+from langchain.tools import CurrentWebPageTool
 from langchain.tools import DuckDuckGoSearchRun
+from langchain.tools import ExtractHyperlinksTool
+from langchain.tools import ExtractTextTool
+from langchain.tools import GetElementsTool
+from langchain.tools import NavigateBackTool
+from langchain.tools import NavigateTool
 from langchain.tools import PubmedQueryRun
 
 
@@ -14,6 +21,15 @@ def main():
         DuckDuckGoSearchRun(),
         PubmedQueryRun(),
         ArxivQueryRun(),
+
+        # playwright
+        ClickTool(),
+        CurrentWebPageTool(),
+        ExtractHyperlinksTool(),
+        ExtractTextTool(),
+        GetElementsTool(),
+        NavigateBackTool(),
+        NavigateTool(),
     ]
     agent = initialize_agent(tools=tools, llm=llm, agent=AgentType.OPENAI_FUNCTIONS, verbose=True)
     while True:
