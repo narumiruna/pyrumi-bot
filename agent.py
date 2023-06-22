3f3efff0b9ecde027271293e362d25370c9e8818
なるみ <toucans-cutouts0f@icloud.com>
2023-06-23 01:14:33 +0800

    refactor: refactor agent initialization and API calls
    - Add WolframAlpha API query tool
    - Remove duplicate ArxivQueryRun tool initialization
    - Rename agent method from `agent` to `run` for consistency
    - Change agent initialization to use AgentType.OPENAI_FUNCTIONS
    - Remove unused import statements
    
    Signed-off-by: なるみ <toucans-cutouts0f@icloud.com>

--- agent.py
+++ agent.py
@@ -5,21 +5,24 @@ from langchain.chat_models import ChatOpenAI
 from langchain.tools import ArxivQueryRun
 from langchain.tools import DuckDuckGoSearchRun
 from langchain.tools import PubmedQueryRun
+from langchain.tools import WolframAlphaQueryRun
+from langchain.utilities.wolfram_alpha import WolframAlphaAPIWrapper
 
 
 def main():
     load_dotenv()
     llm = ChatOpenAI(model_name="gpt-3.5-turbo-0613")
     tools = [
+        ArxivQueryRun(),
         DuckDuckGoSearchRun(),
         PubmedQueryRun(),
-        ArxivQueryRun(),
+        WolframAlphaQueryRun(api_wrapper=WolframAlphaAPIWrapper()),
     ]
     agent = initialize_agent(tools=tools, llm=llm, agent=AgentType.OPENAI_FUNCTIONS, verbose=True)
     while True:
         try:
             question = input("User: ")
-            resp = agent(question)
+            resp = agent.run(question)
             print('Agent:', resp)
         except KeyboardInterrupt:
             break
